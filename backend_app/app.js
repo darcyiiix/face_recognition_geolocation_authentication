0c36bced0f9f120f6847d39ef20498e95b7f375c
darcyiiix <122741372+darcyiiix@users.noreply.github.com>
2024-02-11 12:36:09 +0500

    Update app.js

--- backend_app/app.js
+++ backend_app/app.js
@@ -17,9 +17,6 @@ const path = require('path')
 const app = express()
 app.use(cors())
 app.use(express.json({limit: '5mb'}))
-const corsOptions = {
-    origin: '*',
-}
 app.use(express.static('public'))
 
 
